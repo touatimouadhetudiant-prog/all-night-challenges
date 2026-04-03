e22fe06335fbd3a7d00eecfc41ffb1429ab4f4b5
Mouadh <touatimouadh.etudiant@gmail.com>
2026-04-03 23:25:38 +0200

    fix fetch url

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -101,7 +101,7 @@ function App() {
     }
 
     try {
-      const response = await fetch('http://localhost:5000/api/register', {
+      const response = await fetch(`${API_URL}/api/register`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
