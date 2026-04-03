fb9f869b966b56c30dbad6a90c61d75713c8b774
Mouadh <touatimouadh.etudiant@gmail.com>
2026-04-04 00:29:39 +0200

    fix vercel routes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import SuccessPage from './SuccessPage';
+import AdminPage from './AdminPage';
 import './index.css';
-import AdminPage from "./AdminPage";
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
